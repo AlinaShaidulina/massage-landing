0fd0d666b50f0e9cfd4beb5ebd30a16cea8c9919
Alina <alina_nmsu@mail.ru>
2023-08-26 13:27:22 +0500

    Redesign

--- js/index.js
+++ js/index.js
@@ -18,14 +18,38 @@ new Glide('.j-service-carousel', {
         }
     },
     gap: 10,
-    animationDuration: 300,
-    autoplay: 3000
 }).mount()
 
-new Glide('.j-people-carousel', {
+// new Glide('.j-people-carousel', {
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 2,
+//     breakpoints: {
+//         767: {
+//             perView: 1,
+//         },
+//         991: {
+//             perView: 2,
+//         }
+//     },
+//     gap: 10,
+//     animationDuration: 300,
+//     autoplay: 3000
+// }).mount()
+
+new Glide('.j-promotions-carousel', {
     type: 'carousel',
     startAt: 0,
-    perView: 3,
+    perView: 1,
+    gap: 10,
+    animationDuration: 1200,
+    autoplay: 6000
+}).mount()
+
+new Glide('.j-reviews-carousel', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 5,
     breakpoints: {
         767: {
             perView: 1,
